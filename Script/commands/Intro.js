b9e313ed3d08183f0d697e1880f1cdc5bf0dba8a
Msk <sahariyam13@gmail.com>
2026-08-30 23:33:28 +0600

    Update Intro.js

--- Script/commands/Intro.js
+++ Script/commands/Intro.js
@@ -1,13 +1,10 @@
-const axios = require("axios");
-const request = require("request");
-const fs = require("fs-extra");
 const moment = require("moment-timezone");
 
 module.exports.config = {
  name: "intro",
  version: "1.0.0",
  hasPermssion: 0,
- credits: "🔰Rahat Islam🔰",
+ credits: "🔰MSK🔰",
  description: "Show  Info",
  commandCategory: "info",
  usages: "info",
@@ -17,28 +14,22 @@ module.exports.config = {
 module.exports.run = async function({ api, event }) {
  const time = moment().tz("Asia/Dhaka").format("DD/MM/YYYY hh:mm:ss A");
 
- const callback = () => api.sendMessage({
+ return api.sendMessage({
  body: `
 ┏━━━━━━━━━━━━━━━━┓
 ┃ 🌟𝐎𝐖𝐍𝐄𝐑 𝐈𝐍𝐅𝐎🌟
 ┣━━━━━━━━━━━━━━━━┫
-┃👤𝐍𝐀𝐌𝐄      :𝐑𝐀𝐇𝐀𝐓
+┃👤𝐍𝐀𝐌𝐄      :SHAGARIYAR
 ┃🚹𝐆𝐄𝐍𝐃𝐄𝐑    :𝐌𝐀𝐋𝐄
-┃🎂𝐀𝐆𝐄       :𝟏𝟔
+┃🎂𝐀𝐆𝐄       :𝟏8
 ┃🕌𝐑𝐄𝐋𝐈𝐆𝐈𝐎𝐍 : 𝐈𝐒𝐋𝐀𝐌
-┃🏫𝐄𝐃𝐔𝐂𝐀𝐓𝐈𝐎𝐍 :বয়ড়া ইসরাইল
-┃🏡𝐀𝐃𝐃𝐑𝐄𝐒𝐒 :জামালপুর,বাংলাদেশ
+┃🏫𝐄𝐃𝐔𝐂𝐀𝐓𝐈𝐎𝐍 :SSC 26
+┃🏡𝐀𝐃𝐃𝐑𝐄𝐒𝐒 : DHAKA
 ┣━━━━━━━━━━━━━━━━┫
-┃𝐓𝐈𝐊𝐓𝐎𝐊 :@where.is.she15
 ┃📢𝐓𝐄𝐋𝐄𝐆𝐑𝐀𝐌 :দিবো না🥴🤪
 ┃🌐𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊 :বায়ো-তে আছে
 ┣━━━━━━━━━━━━━━━━┫
 ┃🕒𝐔𝐏𝐃𝐀𝐓𝐄𝐃 𝐓𝐈𝐌𝐄 :${time}
-┗━━━━━━━━━━━━━━━━┛`,
- attachment: fs.createReadStream(__dirname + "/cache/owner.jpg")
- }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/owner.jpg"));
-
- return request("https://i.imgur.com/FJI61jS.jpeg")
- .pipe(fs.createWriteStream(__dirname + '/cache/owner.jpg'))
- .on('close', () => callback());
+┗━━━━━━━━━━━━━━━━┛`
+ }, event.threadID);
 };
