f011fbf93d9bdd539d71edf8209cf4557d250033
Msk <sahariyam13@gmail.com>
2026-08-31 00:06:28 +0600

    Update approve.js

--- Script/commands/approve.js
+++ Script/commands/approve.js
@@ -5,7 +5,7 @@ module.exports.config = {
   name: "approve",
   version: "1.8",
   hasPermssion: 2,
-  credits: "🔰𝐑𝐀𝐇𝐀𝐓 𝐈𝐒𝐋𝐀𝐌🔰",
+  credits: "🔰MSK🔰",
   description: "Approve group, show list & reply number to remove",
   commandCategory: "Admin",
   usages: "!approve <tid> <2day/2month/2year> | !approve box",
@@ -68,7 +68,7 @@ module.exports.run = async ({ api, event, args }) => {
     let msg = "";
     msg += "╭─‣ 𝐀𝐏𝐏𝐑𝐎𝐕𝐄𝐃 𝐆𝐑𝐎𝐔𝐏𝐒\n";
     msg += `├‣ 𝐓𝐎𝐓𝐀𝐋 : ${data.length}\n`;
-    msg += "├‣ 🔰𝐑𝐀𝐇𝐀𝐓 𝐈𝐒𝐋𝐀𝐌🔰\n";
+    msg += "├‣ 🔰SHAHARIYAR🔰\n";
     msg += "╰────────────◊\n";
     msg += "  ─────×\n";
 
