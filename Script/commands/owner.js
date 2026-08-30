8018c1c3a85a1a2d10ede0975633e926b2f7dcfc
Msk <sahariyam13@gmail.com>
2026-08-31 01:17:04 +0600

    Update owner.js

--- Script/commands/owner.js
+++ Script/commands/owner.js
@@ -1,12 +1,9 @@
-const request = require("request");
-const fs = require("fs-extra");
-
 module.exports.config = {
   name: "owner",
   version: "1.0.1",
   hasPermssion: 0,
-  credits: "🔰𝐑𝐀𝐇𝐀𝐓 𝐈𝐒𝐋𝐀𝐌🔰",
-  description: "Show Owner Info with styled box & random photo",
+  credits: "🔰MSK🔰",
+  description: "Show Owner Info with styled box",
   commandCategory: "Information",
   usages: "owner",
   cooldowns: 2
@@ -14,46 +11,23 @@ module.exports.config = {
 
 module.exports.run = async function ({ api, event }) {
 
-  
   const info = `
 ╔═══════════════✿
 ║ ✨ 𝗢𝗪𝗡𝗘𝗥 𝗜𝗡𝗙𝗢 ✨
 ╠═══════════════✿
-║ 👑 𝗡𝗮𝗺𝗲 : 𝗥𝗮𝗵𝗮𝘁 𝗜𝘀𝗹𝗮𝗺
-║ 🧸 𝗡𝗶𝗰𝗸 𝗡𝗮𝗺𝗲 : 𝗥𝗮𝗵𝗮𝘁
-║ 🎂 𝗔𝗴𝗲 : 16
+║ 👑 𝗡𝗮𝗺𝗲 : SHAHARIYAR
+║ 🧸 𝗡𝗶𝗰𝗸 𝗡𝗮𝗺𝗲 : MSK
+║ 🎂 𝗔𝗴𝗲 : 18
 ║ 💘 𝗥𝗲𝗹𝗮𝘁𝗶𝗼𝗻 : 𝗦𝗶𝗻𝗴𝗹𝗲
 ║ 🎓 𝗣𝗿𝗼𝗳𝗲𝘀𝘀𝗶𝗼𝗻 : 𝗦𝘁𝘂𝗱𝗲𝗻𝘁
-║ 🏡 𝗔𝗱𝗱𝗿𝗲𝘀𝘀 : 𝗝𝗮𝗺𝗮𝗹𝗽𝘂𝗿
+║ 🏡 𝗔𝗱𝗱𝗿𝗲𝘀𝘀 : DHAKA
 ╠═══════════════✿
 ║ 🔗 𝗖𝗢𝗡𝗧𝗔𝗖𝗧 𝗟𝗜𝗡𝗞𝗦
 ╠═══════════════✿
 ║ 📘 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 :
-║ fb.com/61582708907708
-║ 💬 𝗠𝗲𝘀𝘀𝗲𝗻𝗴𝗲𝗿 :
-║ m.me/61582708907708
+║ https://www.facebook.com/M.S.K.14k
 ╚═══════════════✿
 `;
 
-  const images = [
-    "https://i.imgur.com/G8wZwUB.jpeg",
-    "https://i.imgur.com/942vNzR.jpeg",
-    "https://i.imgur.com/viT3o6b.jpeg",
-    "https://i.imgur.com/btn02Xz.jpeg"
-  ];
-
-  const randomImg = images[Math.floor(Math.random() * images.length)];
-
-  const callback = () => api.sendMessage(
-    {
-      body: info,
-      attachment: fs.createReadStream(__dirname + "/cache/owner.jpg")
-    },
-    event.threadID,
-    () => fs.unlinkSync(__dirname + "/cache/owner.jpg")
-  );
-
-  return request(encodeURI(randomImg))
-    .pipe(fs.createWriteStream(__dirname + "/cache/owner.jpg"))
-    .on("close", () => callback());
+  return api.sendMessage(info, event.threadID, event.messageID);
 };
